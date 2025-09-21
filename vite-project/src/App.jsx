600c31ce9ffb8e52cc4b159f776a1cc6ea21bf1a
Laiba60 <laibasaeed026@gmail.com>
2025-09-20 21:33:25 -0700

    add play pause function

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import Sidebar from './components/Sidebar'
-import Player from './components/player'
-import Display from './components/Display'
+import React, { useContext } from 'react';   // <-- import useContext
+import Sidebar from './components/Sidebar';
+import Player from './components/Player';    // also check filename case
+import Display from './components/Display';
+import { PlayerContext } from './context/PlayerContext'; // <-- import context
+
 const App = () => {
+  const { audioRef,track } = useContext(PlayerContext); // <-- destructure audioRef from context
+
   return (
     <div className="h-screen bg-black"> 
-    <div className="h-[90%] flex">
-      <Sidebar/>
-      <Display/>
+      <div className="h-[90%] flex">
+        <Sidebar />
+        <Display />
+      </div>
+      <Player />
+      <audio ref={audioRef} src={track.file} preload="auto"></audio>
     </div>
-    <Player/>
-    </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -4,8 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+import PlayerContextProvider from "./context/PlayerContext";
+
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+  <PlayerContextProvider>
     <App />
+  </PlayerContextProvider>
   </BrowserRouter>
 );
