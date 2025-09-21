148674b2151ab802181c31835e092a3d422d1fb0
Laiba60 <laibasaeed026@gmail.com>
2025-09-20 20:32:00 -0700

    Add design

--- vite-project/src/components/Display.jsx
+++ vite-project/src/components/Display.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
-import { Routes, Route } from "react-router-dom";
-import DisplayHome from './DisplayHome';
-import DisplayAlbum from './DisplayAlbum';
-const Display = () => {
-  return (
-    <div className="w-[100%] m-2 px-6 rounded bg-[#121212] h-[85%] text-white overflow-auto lg:w-[75%] lg:ml-0">
-    <Routes>
-        <Route path='/' element={<DisplayHome/>}/>
-         <Route path='/album/:id' element={<DisplayAlbum/>}/>
-    </Routes>
-    </div>
-  )
-}
+import React, { useRef, useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import DisplayHome from "./DisplayHome";
+import DisplayAlbum from "./DisplayAlbum";
+import { albumsData } from "../assets/images/assets";
 
-export default Display
+const Display = () => {
+  const displayRef = useRef();
+  const location = useLocation();
+
+  const isAlbum = location.pathname.includes("album");
+  const albumId = isAlbum ? Number(location.pathname.split("/").pop()) : null;
+  const bgColor =
+    isAlbum && albumsData[albumId - 1]
+      ? albumsData[albumId - 1].bgColor
+      : "#121212"; // fallback
+
+  useEffect(() => {
+    if (isAlbum && displayRef.current) {
+      displayRef.current.style.background = `linear-gradient(180deg, ${bgColor} 0%, #121212 100%)`;
+    }
+  }, [isAlbum, bgColor]);
+
+  return (
+    <div
+      ref={displayRef}
+      className="w-[100%] m-2 px-6 rounded bg-[#121212] h-[85%] text-white overflow-auto lg:w-[75%] lg:ml-0"
+    >
+      <Routes>
+        <Route path="/" element={<DisplayHome />} />
+        <Route path="/album/:id" element={<DisplayAlbum />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default Display;
--- vite-project/src/components/DisplayAlbum.jsx
+++ vite-project/src/components/DisplayAlbum.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import NavBar from './NabBar'
 import { useParams } from 'react-router-dom'
-import {albumsData} from '../assets/images/assets'          
+import {albumsData} from '../assets/images/assets'  
+import { assets } from "../assets/images/assets"; 
+import {songsData} from '../assets/images/assets' 
+
 const DisplayAlbum = () => {
     const {id}=useParams();
    const albumData=albumsData[id];
@@ -20,11 +23,39 @@ const DisplayAlbum = () => {
                 <b>Spotify</b>
                 .1,323,154
                 .<b>50 ,songs</b>
+                about 2 hr 30 min
             </p>
 
         </div>
 
     </div>
+   <div className="grid grid-cols-3 sm:grid-cols-4 mt-10 mb-4 pl-2 text-[#a7a7a7]">
+    <p>
+        <b className="mr-4">
+            #
+        </b>
+        Title
+    </p>
+    <p>Album</p>
+    <p className="hidden sm:block">Date Added</p>
+    <img  className="m-auto w-4" src={assets.clock_icon} alt=""/>
+
+   </div>
+   <hr />
+   {songsData.map((item,index)=>(
+    <div key={index} className="grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] bg-[#ffffff2b] cursor-pointer">
+        <p className="text-white">
+        <b className="mr-4 text-[#a7a7a7]">{index+1}</b>
+        <img src={item.image} alt="" className="w-10 inline mr-5"/>
+        {item.name}
+        </p>
+        <p className="text-[15px]">{albumData.name}</p>
+        <p className="text-[15px] hidden sm:block">5 days ago</p>
+        <p className="text-[15px] text-center">{item.duration}</p>
+    </div>
+
+   ))}
+
     </>
   )
 }
