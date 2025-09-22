c5d971f5ef0c9d06b16ca151dbeae1f69d1de61f
Laiba60 <laibasaeed026@gmail.com>
2025-09-22 06:54:15 -0700

    add vertical scrollbar

--- vite-project/src/components/Display.jsx
+++ vite-project/src/components/Display.jsx
@@ -21,15 +21,16 @@ const Display = () => {
   }, [isAlbum, bgColor]);
 
   return (
-    <div
-      ref={displayRef}
-      className="w-[100%] m-2 px-6 rounded bg-[#121212] h-[85%] text-white overflow-auto lg:w-[75%] lg:ml-0"
-    >
-      <Routes>
-        <Route path="/" element={<DisplayHome />} />
-        <Route path="/album/:id" element={<DisplayAlbum />} />
-      </Routes>
-    </div>
+   <div
+  ref={displayRef}
+  className="flex-1 m-2 px-6 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0"
+>
+  <Routes>
+    <Route path="/" element={<DisplayHome />} />
+    <Route path="/album/:id" element={<DisplayAlbum />} />
+  </Routes>
+</div>
+
   );
 };
 
--- vite-project/src/components/DisplayHome.jsx
+++ vite-project/src/components/DisplayHome.jsx
@@ -1,40 +1,48 @@
 import React from 'react'
 import NavBar from './NabBar'
-import {albumsData} from '../assets/images/assets'
+import { albumsData } from '../assets/images/assets'
 import AlbumItem from './AlbumItem'
-import {songsData} from '../assets/images/assets'
+import { songsData } from '../assets/images/assets'
 import SongsItem from "./SongsItem";
-
 
 const DisplayHome = () => {
   return (
-   <>
-   <NavBar/>
-   <div className="my-5 font-bold text-2xl">
-    <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
-    <div className="flex">
-    {albumsData.map((item,index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
-    </div>
-    
-   </div>
- <div className="my-5">
-  <h1 className="mb-4 font-bold text-2xl">Today’s Biggest Hits</h1>
-  <div className="flex ">
-    {songsData.map((item, index) => (
-      <SongsItem
-        key={index}
-        name={item.name}
-         id={item.id} 
-        desc={item.desc}
-        image={item.image}
-        bgColor={item.bgColor} 
-      />
-    ))}
-  </div>
-</div>
+    <>
+      <NavBar />
 
+     
+      <div className="my-5">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        <div className="grid grid-cols-2 gap-4 max-h-64 overflow-y-auto sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+          {albumsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
 
-   </>
+      {/* Today’s Biggest Hits */}
+      <div className="my-5">
+        <h1 className="mb-4 font-bold text-2xl">Today’s Biggest Hits</h1>
+        <div className="grid grid-cols-2 gap-4 max-h-64 overflow-y-auto sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+          {songsData.map((item, index) => (
+            <SongsItem
+              key={index}
+              name={item.name}
+              id={item.id}
+              desc={item.desc}
+              image={item.image}
+              bgColor={item.bgColor}
+            />
+          ))}
+        </div>
+      </div>
+    </>
   )
 }
 
